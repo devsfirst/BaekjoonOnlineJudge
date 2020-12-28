f093f28d3bd7b113e79e2dceb15ba66e10489a13
devsfirst <seokjh7172@naver.com>
2020-12-29 00:08:59 +0900

    2020. 12. 29

---
+++ DynamicProgramming/[14936] 엘리베이터 장난.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int N = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+
+        sol(N, m);
+    }
+
+    public static void sol(int N, int m) {
+        int btnAll = N;
+        int btnEven = N / 2;
+        int btnOdd = N % 2 == 0 ? N / 2 : N / 2 + 1;
+        int btn3k = (N - 1) / 3 + 1;
+        int num = 0;
+
+        if (0 <= m) num++;
+        if (btnAll <= m) num++;
+        if (btnEven <= m) num++;
+        if (btnOdd <= m) num++;
+        if (btn3k <= m) num++;
+        if (btnAll + btn3k <= m) num++;
+        if (btnEven + btn3k <= m) num++;
+        if (btnOdd + btn3k <= m) num++;
+
+        System.out.println(num);
+    }
+}
