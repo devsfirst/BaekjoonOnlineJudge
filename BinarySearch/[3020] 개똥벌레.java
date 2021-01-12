2463b6453e3ec18752654c4e65d7fe0c00bb3aeb
devsfirst <seokjh7172@naver.com>
2021-01-12 16:35:39 +0900

    2021. 01. 12

--- BinarySearch/[3020] 개똥벌레.java
+++ BinarySearch/[3020] 개똥벌레.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,47 +9,66 @@ public class Main {
     private static StringTokenizer st;
     private static int N;
     private static int H;
-    private static int[] height;
-    private static int[] obstacle;
-    private static int minObstacle;
-    private static int num = 2;
+    private static int[] a;
+    private static int[] b;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
-        height = new int[N];
-        obstacle = new int[H + 1];
-        minObstacle = N / 2;
+        a = new int[N / 2];
+        b = new int[N / 2];
+        int minObstacle = Integer.MAX_VALUE;
+        int num = 0;
 
-        for (int i = 0; i < N; i += 2) {
-            height[i] = Integer.parseInt(br.readLine());
-            height[i + 1] = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N / 2; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+            b[i] = Integer.parseInt(br.readLine());
         }
 
-        binarySearch(1, H);
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int start, end;
+        int mid;
+        int obstacle, obstacleA, obstacleB;
+        for (int height = 1; height <= H; height++) {
+            obstacleA = 0;
+            obstacleB = 0;
+            // 석순
+            start = 0;
+            end = N / 2 - 1;
+            while (start <= end) {
+                mid = (start + end) / 2;
+                if (a[mid] >= height) {
+                    obstacleA = a.length - mid;
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+
+            // 종유석
+            start = 0;
+            end = N / 2 - 1;
+            while (start <= end) {
+                mid = (start + end) / 2;
+                if (height + b[mid] > H) {
+                    obstacleB = b.length - mid;
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+
+            obstacle = obstacleA + obstacleB;
+            if (obstacle < minObstacle) {
+                minObstacle = obstacle;
+                num = 1;
+            }
+            else if (obstacle == minObstacle) num++;
+        }
 
         System.out.println(minObstacle + " " + num);
-    }
-
-    public static void binarySearch(int start, int end) {
-        int mid = (start + end) / 2;
-        if (start > end || mid == start || mid == end) return;
-
-        for (int i = 0; i < N; i += 2) {
-            // 석순
-            if (height[i] >= mid) obstacle[mid]++;
-            // 종유석
-            if (height[i + 1] <= mid) obstacle[mid]++;
-        }
-
-        if (obstacle[mid] == minObstacle) {
-            num++;
-        } else {
-            minObstacle = obstacle[mid];
-            num = 1;
-        }
-        binarySearch(start, mid);
-        binarySearch(mid + 1, end);
     }
 }
