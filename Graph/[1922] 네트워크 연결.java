fd80412b7642df0183e2069c0bd2ade944c6ef32
devsfirst <seokjh7172@naver.com>
2021-02-01 10:20:10 +0900

    2021. 02. 01

--- Graph/[1922] 네트워크 연결.java
+++ Graph/[1922] 네트워크 연결.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -57,10 +56,6 @@ public class Main {
         if (set[i] == i) return i;
         set[i] = find(set[i]);
         return set[i];
-    }
-
-    private static boolean isSameSet(int a, int b) {
-        return find(a) == find(b);
     }
 
     private static class Edge implements Comparable<Edge> {
